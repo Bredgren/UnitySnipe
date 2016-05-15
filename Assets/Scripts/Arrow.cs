8ac7a7ab5208075c3b0c45e5d6346b10704a6ff0
Bredgren <brandon.edgren@gmail.com>
2016-05-15 11:45:03 -0700

    Improved arrow collisions.

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -16,6 +16,8 @@ public class Arrow : MonoBehaviour {
 	private AudioSource audioSource;
 
 	private bool stuck = false;
+	private GameObject attachPoint;
+	private Transform frozenTransform;
 	private float minVel = 0.1f;
 
 	void Start() {
@@ -34,45 +36,79 @@ public class Arrow : MonoBehaviour {
 				transform.rotation = Quaternion.FromToRotation(Vector3.up, rb.velocity);	
 			}
 		}
+//		if (stuck) {
+//			transform.position = frozenTransform.position;
+//			transform.rotation = frozenTransform.rotation;
+//		}
 	}
 
 	void OnTriggerEnter(Collider col) {
 		if (stuck) {
 			return; // or we might stick to something else
 		}
-			
+	
 		Arrow a = col.GetComponentInParent<Arrow>();
 		if (a != null && !a.IsStuck()) {
 			stationary = true;
 			GetComponent<Collider>().isTrigger = false;
 			audioSource.PlayOneShot(arrowHitSound);
 			trailParticles.Stop();
-			//hitParticles.Play();
+			hitParticles.Play();
 			return;
 		}
-
+	
 		audioSource.PlayOneShot(hitSound);
-
+	
 		stuck = true;
 		rb.isKinematic = true;
-
+	
 		// Creating an intermediate object prevents weird scalling when reparenting
-		var empty = new GameObject();
-		empty.transform.parent = col.transform;
-		transform.parent = empty.transform;
-
+		attachPoint = new GameObject();
+		attachPoint.transform.parent = col.transform;
+		transform.parent = attachPoint.transform;
+	
 		trailParticles.Stop();
-
-		//hitParticles.Play();
+		hitParticles.Play();
 	}
 
 	void OnCollisionEnter(Collision c) {
+		if (stuck) {
+			return; // or we might stick to something else
+		}
+	
+		Arrow a = c.collider.GetComponentInParent<Arrow>();
+		if (a != null && !a.IsStuck()) {
+			stationary = true;
+			GetComponent<Collider>().isTrigger = false;
+			audioSource.PlayOneShot(arrowHitSound);
+			trailParticles.Stop();
+			hitParticles.transform.position = c.contacts[0].point;
+			hitParticles.Play();
+			return;
+		}
+	
+		audioSource.PlayOneShot(hitSound);
+	
+		stuck = true;
+		rb.isKinematic = true;
+		//rb.detectCollisions = false;
+
+		// Creating an intermediate object prevents weird scaling when reparenting
+		attachPoint = new GameObject();
+		attachPoint.transform.parent = c.transform;
+		transform.parent = attachPoint.transform;
+//		frozenTransform = transform;
+//		rb.constraints = RigidbodyConstraints.FreezeAll;
+	
+		trailParticles.Stop();
+	
 		hitParticles.transform.position = c.contacts[0].point;
 		hitParticles.Play();
 	}
 
 	void Kill() {
 		Destroy(gameObject);
+		Destroy(attachPoint);
 	}
 
 	bool IsStuck() {
